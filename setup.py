d44fe828c2120042ea33587f128d5e7490fe49a2
GOWTHAMAN R <61967645+Gowthaman1401@users.noreply.github.com>
2021-09-07 11:46:07 +0530

    Setup-Writer

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='setup_writer',
+    packages=setuptools.find_packages(),
+    version='1.2.1',
+    license='MIT',
+    description='To Help In Writing setup.py File And Upload Your Package To Pip',
+    author='Gowthaman',
+    author_email='rgngowthaman1@gmail.com',
+    url='https://github.com/Gowthaman1401/Setup-Writer',
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires='>=3.6',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    install_requires=[
+         "colorama",
+    ],
+    entry_points={'console_scripts': ['pip-setup=setup_writer.__main__:main', 'pip-upload=setup_writer.__main__:upload_main']}
+)
